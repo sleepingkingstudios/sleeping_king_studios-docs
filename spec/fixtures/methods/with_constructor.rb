1709d8be8ba8a29a951110ca1e2fd0afe41d3848
Rob Smith <merlin@sleepingkingstudios.com>
2022-10-22 17:19:23 -0400

    Add #constructor flag to MethodObject.

---
+++ spec/fixtures/methods/with_constructor.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Rocketry
+  # You are going to space today.
+  def initialize; end # rubocop:disable Lint/RedundantCopDisableDirective Style/RedundantInitialize
+end
---
+++ spec/fixtures/methods/with_module_initialize.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Space
+  # You are going to space today.
+  def initialize; end # rubocop:disable Lint/RedundantCopDisableDirective Style/RedundantInitialize
+end
