d0a91a2a7cf0c346aff7cec6b590dbe750c03f8c
Rob Smith <merlin@sleepingkingstudios.com>
2022-11-05 01:41:02 -0400

    Strip RuboCop directives from MethodObject#signature.

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -2,9 +2,24 @@
 
 require 'rspec/sleeping_king_studios/concerns/shared_example_group'
 
+require 'sleeping_king_studios/tools/toolbox/mixin'
+
 module Spec::Support
   module Fixtures
     extend RSpec::SleepingKingStudios::Concerns::SharedExampleGroup
+    extend SleepingKingStudios::Tools::Toolbox::Mixin
+
+    module ClassMethods
+      def wrap_context(context_name, *args, **kwargs, &block)
+        return super unless context_name == 'using fixture'
+
+        context %(using fixture "#{args.first}") do
+          include_context 'using fixture', *args, **kwargs
+
+          instance_exec(&block)
+        end
+      end
+    end
 
     shared_context 'using fixture' do |fixture_name, **options| # rubocop:disable RSpec/ContextWording
       let(:fixture) { "#{fixture_name.tr ' ', '_'}.rb" }
