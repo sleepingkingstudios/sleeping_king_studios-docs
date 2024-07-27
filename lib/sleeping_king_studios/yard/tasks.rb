d00298eea763a4466068b7e13049f79c6749a435
Rob Smith <sleepingkingstudios@gmail.com>
2024-07-27 17:15:20 -0400

    Implement Tasks::Generate.

--- lib/sleeping_king_studios/yard/tasks.rb
+++ lib/sleeping_king_studios/yard/tasks.rb
@@ -6,3 +6,5 @@ module SleepingKingStudios::Yard
   # Namespace for CLI tasks, powered by Thor.
   module Tasks; end
 end
+
+require 'sleeping_king_studios/yard/tasks/generate'
--- lib/sleeping_king_studios/yard/tasks/base.rb
+++ lib/sleeping_king_studios/yard/tasks/base.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'thor'
+
 require 'sleeping_king_studios/tools/toolbelt'
 
 require 'sleeping_king_studios/yard/tasks'
@@ -7,11 +9,6 @@ require 'sleeping_king_studios/yard/tasks'
 module SleepingKingStudios::Yard::Tasks
   # Generic base CLI task.
   class Base < Thor
-    # @private
-    def self.exit_on_failure?
-      true
-    end
-
     private
 
     def tools
