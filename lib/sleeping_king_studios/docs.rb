9f10a1b1b8281bee460c4e951a30559926900f3a
Rob Smith <sleepingkingstudios@gmail.com>
2026-09-14 16:16:45 -0400

    Implement SleepingKingStudios::Docs.initializer.

--- lib/sleeping_king_studios/docs.rb
+++ lib/sleeping_king_studios/docs.rb
@@ -1,26 +1,39 @@
 # frozen_string_literal: true
 
+require 'cuprum/cli'
+require 'sleeping_king_studios/tools'
+
 # Hic iacet Arthurus, rex quondam, rexque futurus.
 module SleepingKingStudios
   # Tooling for working with YARD documentation.
   module Docs
-    class << self
-      # @return [String] the absolute path to the gem directory.
-      def gem_path
-        pattern = /#{File.join('', 'lib', 'sleeping_king_studios', '')}?\z/
-
-        __dir__.sub(pattern, '')
-      end
-
-      # @return [String] The current version of the gem.
-      def version
-        VERSION
-      end
-    end
-
     autoload :Data,   'sleeping_king_studios/docs/data'
     autoload :Errors, 'sleeping_king_studios/docs/errors'
     autoload :Jekyll, 'sleeping_king_studios/docs/jekyll'
     autoload :Yard,   'sleeping_king_studios/docs/yard'
+
+    @initializer = SleepingKingStudios::Tools::Toolbox::Initializer.new do
+      Cuprum::Cli.initializer.call
+
+      SleepingKingStudios::Tools.initializer.call
+    end
+
+    # @return [String] the absolute path to the gem directory.
+    def self.gem_path
+      pattern = /#{File.join('', 'lib', 'sleeping_king_studios', '')}?\z/
+
+      __dir__.sub(pattern, '')
+    end
+
+    # @return [SleepingKingStudios::Tools::Toolbox::Initializer] the initializer
+    #   for the module.
+    def self.initializer
+      @initializer
+    end
+
+    # @return [String] The current version of the gem.
+    def self.version
+      VERSION
+    end
   end
 end
